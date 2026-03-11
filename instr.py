864444bb0e5ce1ef539974e0d7437896d94a33db
mauriciolinares2003 <mauriciolinares2003@gmail.com>
2026-03-11 16:15:36 -0400

    M7L3 cambios

--- instr.py
+++ instr.py
@@ -1,40 +1,42 @@
 from PyQt5.QtCore import QTime
 
-win_x, win_y = 200, 100
+
+win_x, win_y = 100, 100
 win_width, win_height = 1000, 600
 
-txt_hello = 'Welcome to the Health status detection program!'
-txt_next = 'Start'
-txt_instruction = ('This application allows you to use the Rufier test to make an initial diagnosis of your health.\n'
-                    'The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion.\n'
-                    'The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds; \n'
-                    'then, within 45 seconds, the subject performs 30 squats.\n'
-                    'When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds\n'
-                    'and then for the last 15 seconds of the first minute of the recovery period.\n'
-                    'Important! If you feel unwell during the test (dizziness,\n'
-                    'tinnitus, shortness of breath, etc.), stop the test and consult a physician.' )
-txt_title = 'Health'
-txt_name = 'Enter Your full name:'
-txt_hintname = "Full name"
+
+txt_hello = '¡Bienvenido al Programa de detección de estado de salud!'
+txt_next = 'Iniciar'
+txt_instruction = ('Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+                   'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco durante el esuferzo físico.\n'
+                   'El sujeto se tumba en posición supina durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos;\n'
+                   'luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
+                   'Cuando el ejercicio termina, el sujeto se tumba y se toman sus pulsaciones de nuevo durante 15 segundos\n'
+                   'y luego durante los últimos 15 segundos del primer minuto del periodo de recuperación.\n'
+                   '¡Importante! Si no se siente bien durante la prueba (mareos,\n'
+                   'tinnitus, falta de respiración, etc.), detenga la prueba y consulte con un médico.' )
+txt_title = 'Salud'
+txt_name = 'Introduzca su nombre completo:'
+txt_hintname = "Nombre completo"
 txt_hintage = "0"
-txt_test1 = 'Lie on your back and take your pulse for 15 seconds. Click the "Start first test" button to start the timer.\nWrite down the result in the appropriate field.'
-txt_test2 = 'Perform 30 squats in 45 seconds. To do this, click the "Start doing squats" button\nto start the squat counter.'
-txt_test3 = 'Lie on your back and take your pulse for the first 15 seconds of the minute, then for the last 15 seconds of the minute.\nPress the "Start final test" button to start the timer.\nThe seconds that should be measured are indicated in green and the minutes that should not be measured are indicated in black. Write down the results in the appropriate fields.'
-txt_sendresults = 'Send the results'
+txt_test1 = 'Acuéstese y tome su pulso durante 15 segundos. Haga clic en el botón "Iniciar primera prueba" para iniciar el temporizador.\nEscriba el resultado en el campo adecuado.'
+txt_test2 = 'Realice 30 sentadillas durante 45 segundos. Para hacer esto, haga clic en el botón "Empezar a hacer sentadillas"\npara iniciar el contador de sentadillas.'
+txt_test3 = 'Acuéstese y tome su pulso los primeros 15 segundos del minuto, luego durante los últimos 15 segundos del minuto.\nPresione el botón "Iniciar prueba final" para iniciar al temporizador.\nLos segundos que deberían medirse se indican en verde y los segundos que no deben medirse se indican en negro. Escriba los resultados en los campos adecuados.'
+txt_sendresults = 'Enviar los resultados'
 txt_hinttest1 = '0'
 txt_hinttest2 = '0'
 txt_hinttest3 = '0'
-txt_starttest1 = 'Start the first test'
-txt_starttest2 = 'Start doing squats'
-txt_starttest3 = 'Start the final test'
+txt_starttest1 = 'Iniciar primera prueba'
+txt_starttest2 = 'Empezar a hacer sentadillas'
+txt_starttest3 = 'Iniciar la prueba final'
 time = QTime(0, 0, 15)
 txt_timer = time.toString("hh:mm:ss")
-txt_age = 'Full years:'
-txt_finalwin = 'Results'
-txt_index = 'Roufier Index: '
-txt_workheart = 'Cardiac performance: '
-txt_res1 = "low. See your doctor right away!"
-txt_res2 = "satisfactory. See your doctor!"
-txt_res3 = "average. It may be worth seeing your doctor to get checked out."
-txt_res4 = "above average"
-txt_res5 = "high"
+txt_age = 'Años completos:'
+txt_finalwin = 'Resultados'
+txt_index = 'Índice de Ruffier: '
+txt_workheart = 'Rendimiento cardíaco: '
+txt_res1 = "bajo. ¡Acuda al médico de inmediato!"
+txt_res2 = "satisfactorio. ¡Vea a su médico!"
+txt_res3 = "promedio. Puede valer la pena ver a su médico para que lo revise."
+txt_res4 = "por encima del promedio"
+txt_res5 = "alto"
