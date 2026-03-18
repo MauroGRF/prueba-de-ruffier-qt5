5bd54bee3284a7ead492453007bb16c28bac51b7
mauriciolinares2003 <mauriciolinares2003@gmail.com>
2026-03-18 16:30:33 -0400

    Documentacion del codigo

--- my_app.py
+++ my_app.py
@@ -1,10 +1,13 @@
 from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
 from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # comprobar los tipos de los valores de entrada
 from PyQt5.QtWidgets import (
-       QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QApplication, 
+       QWidget,
+       QHBoxLayout, 
+       QVBoxLayout, QGridLayout,
        QGroupBox, QRadioButton,
-       QPushButton, QLabel, QListWidget, QLineEdit)
+       QPushButton, QLabel, QListWidget, QLineEdit
+       )
 
 
 from instr import *
@@ -28,37 +31,44 @@ class MainWin(QWidget):
        self.connects()
 
 
-       # inicio:
+       # inicio: mostrar ventana
        self.show()
 
 
    def initUI(self):
        ''' crea elementos gráficos '''
-       self.btn_next = QPushButton(txt_next)
-       self.hello_text = QLabel(txt_hello)
-       self.instruction = QLabel(txt_instruction)
+       self.btn_next = QPushButton(txt_next) # Boton de siguiente
+       self.hello_text = QLabel(txt_hello) # Texto de bienvenida
+       self.instruction = QLabel(txt_instruction) # Texto de instrucciones
 
 
-       self.layout = QVBoxLayout()
+       self.layout = QVBoxLayout() # Organiza los elementos de la ventana (Vertical)
+
+       # addWidget() agregando widgets/elementos al organizador 
        self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
-       self.setLayout(self.layout)
+       
+       self.setLayout(self.layout) # Define el elemento organizador de la ventana 
 
 
   
    def next_click(self):
-       self.tw = TestWin()
-       self.hide()
+       '''Comportamiento de cambio de ventana'''
+       self.tw = TestWin() # Generando una instancia de la ventana de pruebas (second_win.py)
+       self.hide() # Ocultar nuestra ventana
+
    def connects(self):
-       self.btn_next.clicked.connect(self.next_click)
+       # Definir el evento a disparar con el elemento
+       self.btn_next.clicked.connect(self.next_click) 
+
 
 
    ''' establece la apariencia de la ventana (etiqueta, tamaño, ubicación) '''
    def set_appear(self):
-       self.setWindowTitle(txt_title)
-       self.resize(win_width, win_height)
-       self.move(win_x, win_y)
+       self.setWindowTitle(txt_title) # Nombre de la ventana
+       self.resize(win_width, win_height) # Definiendo dimensiones de la ventana (ancho, alto)
+       self.move(win_x, win_y) # El sitio en nuestra pantalla donde se va a mostrar la ventana (posicion x, posicion y)
 
 
 def main():
